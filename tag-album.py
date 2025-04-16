ceb2ca46dc3cbbd58ec1561ce4ab131da390b917
Vu Hung <153976259+vuhung16au@users.noreply.github.com>
2025-04-16 20:30:30 +1000

    fix bug: add ID3 tag if not existsed

--- tag-album.py
+++ tag-album.py
@@ -108,9 +108,14 @@ def set_album_tag(directory, album_name):
                 try:
                     audio = EasyID3(file_path)
                 except ID3NoHeaderError:
-                    audio = ID3(file_path)
-                    audio.add_tags()
-                    audio = EasyID3(file_path)
+                    try:
+                        audio = ID3(file_path)
+                        audio.add_tags()
+                        audio.save()
+                        audio = EasyID3(file_path)
+                    except Exception as e:
+                        print(f"Skipping file {file_path}: {e}")
+                        continue
                 audio['album'] = album_name
                 audio.save()
                 print(f"Set album tag for {file_path}")
